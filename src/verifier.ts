f088e9ec047ecfcdd3dc25f7f84dc7d2043acf42
Shivam Gupta <38907728+pyshivam@users.noreply.github.com>
2022-07-02 18:42:08 +0530

    token verifer added

--- src/verifier.ts
+++ src/verifier.ts
@@ -1,13 +1,61 @@
 /* eslint-disable prefer-const */
-import { decode } from '@tsndr/cloudflare-worker-jwt';
 
-export function verifyTokenId(token: string) {
+import {  jwtVerify,  decodeProtectedHeader, importX509 } from "jose";
+
+export interface decodedToken {
+  header: {alg: string | 'RS256', kid: string, typ: string | 'JWT'} | unknown,
+  payload: {
+    iss: string,
+    aud: string,
+    auth_time: number,
+    user_id: string,
+    sub: string,
+    iat: 1656765300,
+    exp: 1656768900,
+    email: string,
+    email_verified: false,
+    firebase: {
+      identities: {
+        email: [
+          string
+        ]
+      },
+      sign_in_provider: string
+    }
+  } | unknown
+ }
+
+export interface tokenVerification { error: string, isValid: boolean, decoded: decodedToken}
+
+export async function verifyTokenId(token: string, issuer: string, audience: string): Promise<tokenVerification> {
   let isValid = false;
-  let decoded = {};
   let error = null;
+  let decoded: decodedToken;
+  const header = decodeProtectedHeader(token);
 
-  console.log(`[token]: `, decode(token));
+  const data = await fetch(
+    'https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com',
+  );
+  const publicKeys = await data.json();
+  let secret = publicKeys[header.kid]
+  const ecPublicKey = await importX509(secret, header.alg)
 
+  try {
+    let {protectedHeader, payload} = await jwtVerify(token, ecPublicKey, {
+      issuer,
+      audience
+    });
+    isValid = true
+    decoded = {
+      header: JSON.parse(JSON.stringify(protectedHeader)),
+      payload
+    }
+
+  } catch (err) {
+    console.error(err.message);
+    isValid = false
+    error = err.message
+  }
   return {
     error,
     isValid,
