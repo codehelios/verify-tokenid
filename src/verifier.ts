44ef244ffb54f1e49c7d70e796baafe794472eaa
Shivam Gupta <38907728+pyshivam@users.noreply.github.com>
2022-07-02 19:57:15 +0530

    jose added but core added as its

--- src/verifier.ts
+++ src/verifier.ts
@@ -1,33 +1,45 @@
 /* eslint-disable prefer-const */
 
-import {  jwtVerify,  decodeProtectedHeader, importX509 } from "jose";
+import {
+  jwtVerify,
+  decodeProtectedHeader,
+  importX509,
+} from './browser/index.js';
 
 export interface decodedToken {
-  header: {alg: string | 'RS256', kid: string, typ: string | 'JWT'} | unknown,
-  payload: {
-    iss: string,
-    aud: string,
-    auth_time: number,
-    user_id: string,
-    sub: string,
-    iat: 1656765300,
-    exp: 1656768900,
-    email: string,
-    email_verified: false,
-    firebase: {
-      identities: {
-        email: [
-          string
-        ]
-      },
-      sign_in_provider: string
-    }
-  } | unknown
- }
+  header: { alg: string | 'RS256'; kid: string; typ: string | 'JWT' } | unknown;
+  payload:
+    | {
+        iss: string;
+        aud: string;
+        auth_time: number;
+        user_id: string;
+        sub: string;
+        iat: 1656765300;
+        exp: 1656768900;
+        email: string;
+        email_verified: false;
+        firebase: {
+          identities: {
+            email: [string];
+          };
+          sign_in_provider: string;
+        };
+      }
+    | unknown;
+}
 
-export interface tokenVerification { error: string, isValid: boolean, decoded: decodedToken}
+export interface tokenVerification {
+  error: string;
+  isValid: boolean;
+  decoded: decodedToken;
+}
 
-export async function verifyTokenId(token: string, issuer: string, audience: string): Promise<tokenVerification> {
+export async function verifyTokenId(
+  token: string,
+  issuer: string,
+  audience: string,
+): Promise<tokenVerification> {
   let isValid = false;
   let error = null;
   let decoded: decodedToken;
@@ -37,24 +49,23 @@ export async function verifyTokenId(token: string, issuer: string, audience: str
     'https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com',
   );
   const publicKeys = await data.json();
-  let secret = publicKeys[header.kid]
-  const ecPublicKey = await importX509(secret, header.alg)
+  let secret = publicKeys[header.kid];
+  const ecPublicKey = await importX509(secret, header.alg);
 
   try {
-    let {protectedHeader, payload} = await jwtVerify(token, ecPublicKey, {
+    let { protectedHeader, payload } = await jwtVerify(token, ecPublicKey, {
       issuer,
-      audience
+      audience,
     });
-    isValid = true
+    isValid = true;
     decoded = {
       header: JSON.parse(JSON.stringify(protectedHeader)),
-      payload
-    }
-
+      payload,
+    };
   } catch (err) {
     console.error(err.message);
-    isValid = false
-    error = err.message
+    isValid = false;
+    error = err.message;
   }
   return {
     error,
