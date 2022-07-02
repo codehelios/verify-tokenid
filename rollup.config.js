1ee7e24ed87d70cc70bfcdb3772c257274d560d4
Shivam Gupta <38907728+pyshivam@users.noreply.github.com>
2022-07-02 21:29:37 +0530

    rollup bundler added to remove unused code

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import typescript from "@rollup/plugin-typescript"
+import { existsSync, mkdirSync } from "fs";
+import { dirname } from "path";
+// import { terser } from "rollup-plugin-terser"
+
+const dir = "build"
+
+function isDev() {
+  return !!process.argv.find(el => el === "--dev")
+}
+
+function ensureDirectoryExistence(filePath) {
+  var dr = dirname(filePath);
+  if (existsSync(dr)) {
+    return true;
+  }
+  ensureDirectoryExistence(dr);
+  mkdirSync(dr);
+}
+
+ensureDirectoryExistence(dir)
+
+export default {
+
+  input: "./src/index.ts",
+  output: {
+    dir: dir,
+    format: "esm",
+    sourcemap: isDev()
+  },
+  plugins: [
+    typescript({
+      tsconfig: isDev() ? "./tsconfig.json" : "tsconfig.release.json"
+    }),
+    // !isDev() ? terser() : null
+  ]
+
+}
